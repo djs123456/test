b1832722a4718a959659b0c8b65a8efbd8ea5200
e <1303417066@qq.com>
2020-04-06 20:47:00 +0800

    测试6

--- src/demo.js
+++ src/demo.js
@@ -7,7 +7,7 @@ function get() {
         .then(response => {
             if (response){
                 console.log(123)
-                console.log(456)
+                console.log(890)
                 console.log(789)
                 // promise异步编程
                 // 先获取api的数据打印出来，再获取api2的数据打印出来
